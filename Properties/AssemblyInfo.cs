73a4e1cb62a57a3c9b388ccfb172aa5da984533e
WulfMarius <WulfMarius@users.noreply.github.com>
2018-06-24 09:28:16 +0200

    updated for TLD V1.34

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Preselect-Struggle-Weapon")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("© 2018 WulfMarius")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
